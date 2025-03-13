60cd5344333339ba83dd05a8d4d683c54a946697
isnastish <paukden12@gmail.com>
2025-03-13 16:17:12 +0100

    added CQRS pattern

--- internal/adapters/user_postgres_repository.py
+++ internal/adapters/user_postgres_repository.py
@@ -1,4 +1,21 @@
 from ..ports.user_repository import UserRepository
+from ..domain.user import User
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from sqlalchemy import Connection
+
 
 class PostgresUserRepository(UserRepository):
-    pass
+    def __init__(self, conn: "Connection"):
+        self._conn = conn
+
+    async def get_user_by_email(self, email: str) -> User:
+        query = "SELECT * FROM"
+    
+    async def get_user_by_id(self, id: str) -> User:
+        pass
+
+    async def add_user(self, user: User) -> None:
+        pass
